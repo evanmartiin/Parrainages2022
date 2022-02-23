a49572fbff1b9cf5fe404eec1e5bcf26f24f012a
evanmartiin <evan.martiin29@gmail.com>
2022-02-23 19:32:09 +0100

    feat: v1

--- mobile/src/views/Home.vue
+++ mobile/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <Hero v-if="!loading" :candidates="candidates"/>
-    <button class="add-button">Add to home screen</button>
     <CandidatesList :candidates="candidates" :loading="loading" :error="error"/>
   </div>
 </template>
@@ -60,46 +59,6 @@ export default {
       loading,
       error
     };
-  },
-  mounted() {
-    let deferredPrompt;
-    const addBtn = document.getElementsByClassName('add-button')[0];
-    addBtn.style.display = 'none';
-    
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI to notify the user they can add to home screen
-      addBtn.style.display = 'block';
-    
-      addBtn.addEventListener('click', () => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-              console.log('User accepted the A2HS prompt');
-            } else {
-              console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-          });
-      });
-    });
-
   }
 }
-
 </script>
-
-<style>
-.add-button {
-  position: absolute;
-  top: 1px;
-  left: 1px;
-}
-</style>
