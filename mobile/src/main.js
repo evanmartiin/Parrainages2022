8333b2500d14158d3bc9c9f298637a118ed977e7
evanmartiin <evan.martiin29@gmail.com>
2022-02-24 18:15:18 +0100

    feat(data): update data

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+//
 
 createApp(App).use(router).mount('#app')
